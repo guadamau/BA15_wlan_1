d2ff10670ef7e652ddf39892550043adce73eed3
guadamau <guadamau@students.zhaw.ch>
2015-04-24 17:01:56 +0200

    UDP/Ethernet comparison without conclusion / histogram test (commented out in Resultate.lyx

---
+++ doc/_histo/szenario_01/copyoverall/addtitles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in $(find . | grep "meas_ove"); do
+    INT_FILE_NAME=$(echo "$i" | rev | cut -d'/' -f1 | rev)
+    INT_FILE=$i
+    DIR_NAME=$(dirname $i)
+    DIR_OUT=$(echo ${DIR_NAME}/out/${INT_FILE_NAME})
+    sed -i -e '1i${DIR_NAME}\***********************\' $i
+done
